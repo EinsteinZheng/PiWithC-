b7a78d9ef0b6b1a372b5516330a527c630d21701
Einstein Zheng <mocoder@icloud.com>
2019-01-16 20:16:57 +0800

    Initial Commit

---
+++ cpppi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpppi
+//
+//  Created by Einstein Zheng on 1/16/19.
+//  Copyright © 2019 Einstein Zheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
